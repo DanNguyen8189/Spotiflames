bb0dd40a7a98c530510196e369b6f665aeff024d
DanNguyen8189 <dannguyen8189@gmail.com>
2020-04-13 16:49:28 -0700

    Top artists page functionality working, need styling now

--- client/src/components/TopArtists.vue
+++ client/src/components/TopArtists.vue
@@ -1,20 +1,27 @@
 <template>
     <div class='topartists'>
+      <h1>Top Artists</h1>
+      <div id = "top">
+      <button v-on:click="page_state = 'short'">Past Month</button>
+      <button v-on:click="page_state = 'medium'">Past 6 Months</button>
+      <button v-on:click="page_state = 'long'">Ever</button>
+      </div>
       <template v-if="this.userArtistsShort">
-        <h1>Top Artists</h1>
+        
       <!--<img :src="this.user.images[0].url" alt="profile_picture" class="profile_pic">-->
         <!--<img :src="this.userArtistsShort.items[0].images[2].url" alt="artist_picture">-->
-        <div v-for="n in 10" :key="n">
+        <div v-for="n in 20" :key="n" class="list">
           <!--<img v-bind:src="`this.userArtistsShort.items[0].images[2].url`">-->
-          <!--<img src="https://i.scdn.co/image/15616ceb5c8c7e88aa07cb2f406a2b722a36fcd0">-->
+          <!--<img src="https://i.scdn.co/image/6c785e6be62ae82a5222578045439fc235086f64">-->
           <!--<img :src="this.userArtistsShort.items[0].images[2].url" alt="artist_picture">-->
           <!--<img :src="this.$store.state.topArtistsShort.items[0].images[2].url" alt="artist_picture">-->
+          
           <img :src=getImage(n-1)>
-          <div>{{ getName(n-1) }}</div>
+          <div class='artist-text'>{{ getName(n-1) }}</div>
         </div>
       </template>
       <template v-else>
-        <h1>topartists not found</h1>
+        <h1>loading...</h1>
       </template>
     </div>
 </template>
@@ -25,9 +32,11 @@ export default {
   name: 'TopArtists',
   data () {
     return {
-      msg: 'Top artists page'
+      msg: 'Top artists page',
+      page_state: 'short'
     }
   },
+  // might not need this
   computed: {
     userArtistsShort () {
       return this.$store.getters.getTopArtistsShort
@@ -50,11 +59,27 @@ export default {
         console.log(this.$store.state.topArtistsShort);
       });
     },
+    // get artist image based on the page state
     getImage (index) {
-      return this.$store.getters.getTopArtistsShort.items[index].images[2].url;
+      switch (this.page_state) {
+        case 'short':
+          return this.$store.getters.getTopArtistsShort.items[index].images[2].url;
+        case 'medium':
+          return this.$store.getters.getTopArtistsMedium.items[index].images[2].url;
+        case 'long':
+          return this.$store.getters.getTopArtistsLong.items[index].images[2].url;
+      }
     },
+    // get artist name base on the page state
     getName (index) {
-      return this.$store.getters.getTopArtistsShort.items[index].name;
+      switch (this.page_state) {
+        case 'short':
+          return this.$store.getters.getTopArtistsShort.items[index].name;
+        case 'medium':
+          return this.$store.getters.getTopArtistsMedium.items[index].name;
+        case 'long':
+          return this.$store.getters.getTopArtistsLong.items[index].name;
+      }
     }
   },
   created () {
@@ -64,4 +89,23 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  color: #ff741e;
+}
+#top {
+  display: block;
+}
+.list{
+  display: inline-block;
+  width: 20%;
+  margin: 30px 0;
+}
+img {
+  border-radius: 50%;
+  display: inline-block;
+  border: 4px double #e42c6a;
+}
+.artist-text{
+  color: #fcd02c;
+}
 </style>
